4d0bf040a423ad411723a9a4cf88279007c9a7f5
Dansol79 <danyalv_79@hotmail.com>
2021-10-09 20:01:32 -0300

    primer commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import reactDom from 'react-dom'
+// import ReactDOM from 'react-dom';
+
+  const Elemento = () => <p>Hola Mundo</p>
+
+  reactDom.render( <Elemento/>, document.getElementById('root') )
