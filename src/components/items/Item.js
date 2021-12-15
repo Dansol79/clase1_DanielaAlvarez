a2cdf04f997d014b0f1dced3d46c259495c8e21d
Dansol79 <danyalv_79@hotmail.com>
2021-12-15 18:25:25 -0300

    se actualiza

--- src/components/items/Item.js
+++ src/components/items/Item.js
@@ -1,19 +1,19 @@
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 import "./styloItems.css"
 
 
-const Item = ({ item}) =>{
-    
-        return(
+const Item = ({ item }) => {
 
-        
-                     <Link to={`/articulo_Selec/${item.id}`}>
-                      <img src={ item.imagen} alt="imagPro"  className="stiloimg" /> 
-                      <h3 className="titulo_item" >{ item.name}</h3>
-                      <p className="paragraph_item">$ { item.price}</p>
-                      <button className="boton_vermas"> VER MAS...</button>
-                     </Link>
-            
+        return (
+                <>
+                        <img src={item.imagen} alt="imagPro" className="stiloimg" />
+                        <h3 className="titulo_item" >{item.name}</h3>
+                        <p className="paragraph_item">$ {item.price}</p>
+                        <Link to={`/articulo_Selec/${item.id}`}>
+                                <button className="boton_vermas"> VER MAS...</button>
+                        </Link>
+                </>
+
         )
 
 
